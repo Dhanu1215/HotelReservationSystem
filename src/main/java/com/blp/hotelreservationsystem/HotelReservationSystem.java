588b6ba4dd174c64cd7b8695b296a7d3412d7a7e
Asus <dhanashreehakke7@gmail.com>
2021-11-20 19:09:17 +0530

    Added: welcome message is added

---
+++ src/main/java/com/blp/hotelreservationsystem/HotelReservationSystem.java
@@ -0,0 +1,11 @@
+package com.blp.hotelreservationsystem;
+
+/**
+ * Hotel Reservation System Program.
+ */
+public class HotelReservationSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to hotel reservation system program");
+    }
+}
